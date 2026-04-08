633e4d4ecfa29e73f9f3f9dc0b3df7635721f73d
BaldoAB1995 <andrea.baldin95@gmail.com>
2026-04-08 13:49:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,45 +7,44 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
-try {
-  const userMessage = req.body.message;
+  try {
+    const userMessage = req.body.message;
 
-  if (!userMessage) {
-    return res.status(400).json({ error: "Message missing" });
-  }
-
-  const response = await fetch(
-     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        contents: [
-        {
-          role: "user",
-          parts: [{ text: userMessage }]
-        },
-        {
-          role: "model",
-          parts: [{ 
-          text: "Sei un poeta stilnovista. Rispondi solo in rima, risposte brevi e spiritose."
-        }]
-        }
-        ]
-      })
+    if (!userMessage) {
+      return res.status(400).json({ error: "Message missing" });
     }
-  );
 
-  const data = await response.json();
-  res.json(data);
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          system_instruction: {
+            parts: [{
+              text: "Sei un poeta stilnovista. Rispondi solo in rima, risposte brevi e spiritose."
+            }]
+          },
+          contents: [
+            {
+              role: "user",
+              parts: [{ text: userMessage }]
+            }
+          ]
+        })
+      }
+    );
 
-} catch (err) {
-  console.error(err);
-  res.status(500).json({ error: "Gemini API error" });
-}
+    const data = await response.json();
+    res.json(data);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Gemini API error" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log("Server running on port", PORT);
+  console.log("Server running on port", PORT);
 });
